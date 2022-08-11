109a2e62487300bc08c7daac33e251551bc72bfa
cybermin <sillamin@naver.com>
2022-08-11 14:19:14 +0900

    app

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import pandas as pd
 import numpy as np
 
 st.title('전국 대학 기숙사현황')
+
+with st.sidebar:
+    add_radio = st.radio(
+        "기숙사 융형선택",
+        ("1인실", "2인실","3인실", '4인실')
+    )
+    st.write(add_radio)
